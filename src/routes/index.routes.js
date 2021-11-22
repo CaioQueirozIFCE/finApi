ce7c17cce84808dff80f797fdeb1621c97e4d463
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2021-11-22 09:28:19 -0300

    retirando arquivos não usados

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,13 +2,11 @@ const { Router } = require('express');
 const {verifyIfExistsAccountCpf} = require('../middlewares/VerifyIfExistsAccountCPF');
 const {validateCPFMiddleware} = require('../middlewares/ValidateCPFMiddleware');
 const accountRoutes = require('./account/account.routes');
-const statementRoutes = require('./statement/statement.routes');
 const transactionRoutes = require('./transactions/index.routes');
 
 const router = new Router();
 
 router.use('/account', accountRoutes);
-router.use('/list-statament', validateCPFMiddleware, verifyIfExistsAccountCpf, statementRoutes);
 router.use('/transaction', validateCPFMiddleware, verifyIfExistsAccountCpf, transactionRoutes);
 
 module.exports = router;
